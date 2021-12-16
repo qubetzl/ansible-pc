0db30a971781faee8099b2a7df6d1d82c9c48950
Qubetzl <qubetzl@protonmail.com>
2021-12-16 20:43:45 +0200

    Initial commit

---
+++ 0-setup.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+apt install -y python3-venv python3-pip
+
+[ ! -d "venv" ] && python3 -m venv venv
+source venv/bin/activate
+pip3 install ansible
+
+ansible-galaxy role install -r requirements.yml
+ansible-playbook 0-setup/playbook.yml
---
+++ bootstrap.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+apt update
+apt install -y git
+
+[ ! -d "venv" ] && git clone https://github.com/qubetzl/ansible-pc.git
+pushd ansible-pc
+bash 0-setup.sh
