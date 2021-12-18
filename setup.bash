3691f0b4165d9a625d355f0984fa713ce0df4011
Qubetzl <qubetzl@protonmail.com>
2021-12-19 01:52:05 +0200

    Expermienting with switching users during setup

--- setup.bash
+++ setup.bash
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+if [ "$(id -u)" -eq 0 ]
+then
 bash 0-setup/0-setup.bash
-sudo -i -u qubetzl bash 1-setup/1-setup.bash
+pushd /home/qubetzl/ansible-pc
+sudo -u qubetzl bash setup.sh
+fi
+
+if [ "$(id -u)" -eq 1000 ]
+then
+pushd ansible-pc
+bash 1-setup/1-setup.bash
+fi
