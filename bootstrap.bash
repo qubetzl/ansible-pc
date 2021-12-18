5e537bad74d96e3582fefdff974c9cdc11a558be
Qubetzl <qubetzl@protonmail.com>
2021-12-18 19:46:09 +0200

    Re-organized step structure

--- bootstrap.bash
+++ bootstrap.bash
@@ -6,4 +6,4 @@ apt-get install -y git
 
 [ ! -d "venv" ] && git clone https://github.com/qubetzl/ansible-pc.git
 pushd ansible-pc
-bash 0-setup.bash
+bash setup.bash
---
+++ setup.bash
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+bash 0-setup/0-setup.bash
